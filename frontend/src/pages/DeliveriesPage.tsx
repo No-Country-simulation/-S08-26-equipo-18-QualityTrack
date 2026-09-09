06dddbbbf4381450b2ca805a0a726290032a59a0
Rodrigo Federico Paz <Rodrigopaz52@hotmail.com>
2026-09-09 12:11:15 -0300

    feat(frontend): configurar sistema de rutas y paginas base (#34)

---
+++ frontend/src/pages/DeliveriesPage.tsx
@@ -0,0 +1,8 @@
+export default function DeliveriesPage() {
+    return (
+    <div>
+        <h1>Entregas</h1>
+        <p>Aquí puedes ver todas las entregas.</p>
+    </div>       
+    );
+}
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>Iniciar sesión</h1>
+            <p>Por favor, introduce tus credenciales para acceder a QualityTrack.</p>
+        </div>
+    );
+}
